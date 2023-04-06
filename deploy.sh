bdeb7fb544cde0ca0a218cc39615efc6910969ce
Andrew Heiss <andrew@andrewheiss.com>
2023-04-06 13:57:48 -0400

    Add custom pp_check for ordbetareg

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+REMOTE_HOST="cloud"
+REMOTE_DIR="~/sites/stats/public_html/compassionate-clam"
+REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
+
+echo "Uploading new changes to remote server..."
+echo
+rsync -crvP --exclude '*_cache' --delete _site/ $REMOTE_DEST
