adac716c66b4d665cb2e895be87cb882fd3d4847
Andrew Heiss <andrew@andrewheiss.com>
2023-09-13 01:25:58 -0400

    oops use native pipe

--- R/fun_tables.R
+++ R/fun_tables.R
@@ -1,11 +1,11 @@
 opts_int <- function(x, ...) {
-  x %>% 
+  x |> 
     opt_interactive(use_compact_mode = TRUE, use_highlight = TRUE, ...)
 }
 
 opts_theme <- function(x) {
-  x %>% 
-    opt_table_font(font = "IBM Plex Sans") %>% 
+  x |> 
+    opt_table_font(font = "IBM Plex Sans") |> 
     tab_options(column_labels.font.weight = "bold",
       row_group.font.weight = "bold")
 }
