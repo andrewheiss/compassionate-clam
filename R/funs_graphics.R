bbbb61d0fdde1abe5a6d23024e133a79f6428627
Andrew Heiss <andrew@andrewheiss.com>
2023-12-05 12:55:51 -0500

    Only set default repelled label font if ggrepel is loaded

--- R/funs_graphics.R
+++ R/funs_graphics.R
@@ -1,11 +1,12 @@
 clrs <- MetBrewer::met.brewer("Tam")
 
 set_annotation_fonts <- function() {
-  library(ggrepel)
-  
   ggplot2::update_geom_defaults("label", list(family = "IBM Plex Sans Condensed", fontface = "plain"))
-  ggplot2::update_geom_defaults("label_repel", list(family = "IBM Plex Sans Condensed", fontface = "plain"))
   ggplot2::update_geom_defaults("text", list(family = "IBM Plex Sans Condensed", fontface = "plain"))
+
+  if (isNamespaceLoaded("ggrepel")) {
+    ggplot2::update_geom_defaults("label_repel", list(family = "IBM Plex Sans Condensed", fontface = "plain"))
+  }
 }
 
 theme_ongo <- function(base_size = 11, base_family = "IBM Plex Sans Condensed", prior = FALSE) {
